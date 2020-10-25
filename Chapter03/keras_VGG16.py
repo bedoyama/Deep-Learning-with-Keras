3607a28bbbd101e8996fc213be9f6c9b2ebe9017
mauricio <bedoyama@gmail.com>
2020-10-24 21:37:06 -0500

    VGG16 test with proper files and updated for py36-2020 - note weights files too large to commit

--- Chapter03/keras_VGG16.py
+++ Chapter03/keras_VGG16.py
@@ -1,53 +1,54 @@
-from keras import backend as K
+from keras import backend as k
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 import cv2, numpy as np
 
+
 # define a VGG16 network
 
 def VGG_16(weights_path=None):
     model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(ZeroPadding2D((1, 1), input_shape=(224, 224, 3)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(Flatten())
 
-    #top layer of the VGG net
+    # top layer of the VGG net
     model.add(Dense(4096, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(4096, activation='relu'))
@@ -55,19 +56,23 @@ def VGG_16(weights_path=None):
     model.add(Dense(1000, activation='softmax'))
 
     if weights_path:
+        # model.load_weights(weights_path, by_name=True)
         model.load_weights(weights_path)
 
     return model
 
+
 if __name__ == "__main__":
-    im = cv2.resize(cv2.imread('cat.jpg'), (224, 224)).astype(np.float32)
-    im = im.transpose((2,0,1))
+    im = cv2.resize(cv2.imread('vgg16/cat.jpg'), (224, 224)).astype(np.float32)
+    # im = im.transpose((2, 0, 1))
     im = np.expand_dims(im, axis=0)
-    K.set_image_dim_ordering("th")
+    print(im.shape)
+    k.set_image_data_format("channels_last")
+    # k.set_image_data_format("channels_first")
 
     # Test pretrained model
-    model = VGG_16('/Users/gulli/Keras/codeBook/code/data/vgg16_weights.h5')
+    model = VGG_16('/Users/mauricio/Documents/github/Deep-Learning-with-Keras/Chapter03/vgg16/vgg16_weights.h5')
     optimizer = SGD()
     model.compile(optimizer=optimizer, loss='categorical_crossentropy')
     out = model.predict(im)
-    print np.argmax(out)
+    print(np.argmax(out))
